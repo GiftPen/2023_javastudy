959cc7c564a7949c879f9ddd95970d19f3fed0ea
kimjaehoon <kjh950805@naver.com>
2023-05-29 18:20:39 +0900

    up0529

--- src/day23/com/ict/edu/Ex05_Dialog.java
+++ src/day23/com/ict/edu/Ex05_Dialog.java
@@ -1,10 +1,12 @@
 package day23.com.ict.edu;
 
+import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Ex05_Dialog extends JFrame{
@@ -36,11 +38,91 @@ public class Ex05_Dialog extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		
+		
 		jb1.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// 메세지 다이얼 로그 : 사용자에게 정보를 알리는 역할만 한다.(js(자바스크립트에서는 alert())
+				// JOptionPane.showMessageDialog(parentComponent, message);
+				// JOptionPane.showMessageDialog(jp, "방가방가"); //INFORMATETION_MESSAGE(기본)
+				// JOptionPane.showMessageDialog(getParent(), "방가방가"); // WARNING_MESSAGE
 				
+				// JOptionPane.showMessageDialog(parentComponent, message, title, messageType);
+				// massageType : ERROR_MESSAGGE, INFORMATETION_MESSAGE(기본), WARNING_MESSAGE,
+				//   			 QUESTION_MESSAGE, PLAIN_MESSAGE(없음)
+				   JOptionPane.showMessageDialog(jp, "방가방가", "MESSAGE 창", JOptionPane.PLAIN_MESSAGE);
+				   
+			}
+		});
+		jb2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 확인 다이얼 로그 : 정보를 알리고 사용자의 반응의 결과를 가져온다.(확인(0), 아니오(1), 취소(2) )
+				//				 (js(자바스크립트에서는 confirm())
+				// JOptionPane.showConfirmDialog(parentComponent, message);
+				// int res = JOptionPane.showConfirmDialog(jp, "정말 삭제할까요");
+				// System.out.println(res);
+				
+				// JOptionPane.showConfirmDialog(parentComponent, message, title, optionType);
+				// optionType : YES_NO_CANCEL_OPTION(예,아니오,취소) , YES_NO_OPTION(예,아니오)
+				// 			    OK_CANCEL_OPTION(확인,취소)
+				// JOptionPane.showConfirmDialog(jp, "정말 삭제할까요", "확인창", JOptionPane.YES_NO_OPTION);
+				   JOptionPane.showConfirmDialog(jp, "정말 삭제할까요", "확인창", JOptionPane.OK_CANCEL_OPTION);
+			}
+		});
+		
+		jb3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			// 입력 다이얼 로그 : 사용자에게 정보를 받는 다이얼 로그 (js(자바스크립트에서는 prompt()) // 왠만해선 잘 안쓰이는 것
+			// String msg = JOptionPane.showInputDialog(parentComponent, message);          	 	
+			// String msg = JOptionPane.showInputDialog(jp, "나이 : " );   
+			// System.out.println(msg);
+				
+			// String msg = JOptionPane.showInputDialog(parentComponent, message, 초기값); 
+			   String msg = JOptionPane.showInputDialog(jp, "나이 : " , 17);   
+			}
+		});
+		
+		jb4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			// 옵션 다이얼 로그 : 사용자 다이얼 로그 (사용자가 만드는 다이얼 로그) //앞에 배운것들이 다들어감
+			//	JOptionPane.showOptionDialog(parentComponent, message, Title, optionType , 
+			//								messageType, null, null, null);
+			int res = JOptionPane.showOptionDialog(jp, "방가방가", "사용자 다이얼 로그", 
+					  JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+				System.out.println(res);
+			}
+		});
+		
+		jb5.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 파일 열기 다이얼 로그
+				FileDialog fd = new FileDialog((JFrame)getParent(),"파일 열기^^", FileDialog.LOAD);
+				fd.setVisible(true);
+				// 위치 경로를 알수 있다
+				String msg = fd.getDirectory() + fd.getFile();
+				System.out.println(msg);
+			}
+		});
+		
+		jb6.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 파일 저장 다이얼 로그
+				FileDialog fd = new FileDialog((JFrame)getParent(),"파일 저장^^", FileDialog.SAVE);
+				fd.setVisible(true);
+				// 위치 경로를 알수 있다
+				String msg = fd.getDirectory() + fd.getFile();
+				System.out.println(msg);
 			}
 		});
 		
